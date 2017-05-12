9bc13b152111f9f71b2b81b8d51cef0de4a8ad0d
Matt Raymer <rayme005@d.umn.edu>
2017-05-12 16:17:16 -0500

    Added multiple pages, started implementing CSS

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
+//var express = require('express');
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 
-fs.readFile('page.html', function(err, html) {
-	if (err) {
-		res.writeHead(404, {'Content-Type': 'text/html'});
-		return res.end("404 Not Found");
-	}
-	http.createServer(function(req, res) {  
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(html);
-		res.end();
-	}).listen(8080, '127.0.0.1');
-});
-console.log('Server running at http://127.0.0.1:8080');
+//var app = express();
+//app.set("port", 8080);
+
+// Create a server
+http.createServer(function (request, response) {
+    // Parse the request containing file name
+    var pathname = url.parse(request.url).pathname;
+
+    // Print the name of the file for which request is made.
+    console.log("Request for " + pathname + " received.");
+
+    // Read the requested file content from file system
+    fs.readFile(pathname.substr(1), function (err, data) {
+        if (err) {
+            console.log(err);
+            // HTTP Status: 404 : NOT FOUND
+            // Content Type: text/plain
+            response.writeHead(404, { 'Content-Type': 'text/html' });
+        } else {
+            //Page found	  
+            // HTTP Status: 200 : OK
+            // Content Type: text/plain
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+
+            // Write the content of the file to response body
+            response.write(data.toString());
+        }
+        // Send the response body 
+        response.end();
+    });
+}).listen(8080);
+
+console.log('Server running at http://10.0.0.205:8080');
